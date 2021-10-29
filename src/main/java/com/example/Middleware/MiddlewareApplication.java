3d61a24dc44b5e130436772158f21b864d6ee1dc
Niclas <niclas.selig@web.de>
2021-10-29 11:23:07 +0200

    Hinzufügen von User zu Liste implementiert

--- src/main/java/com/example/Middleware/MiddlewareApplication.java
+++ src/main/java/com/example/Middleware/MiddlewareApplication.java
@@ -1,16 +1,28 @@
 package com.example.Middleware;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class MiddlewareApplication {
+
+	ArrayList<String> userList = new ArrayList<String>(List.of("Niclas","Lukas"));
+	
+
+	
 
 	@GetMapping("/")
 	public String sayHello(){
@@ -25,6 +37,18 @@ public class MiddlewareApplication {
 	@GetMapping("/hello/{parameter}")
 	public String sayParameter(@PathVariable String parameter){
 		return "Hello, das ist der Rest Call mit dem Parameter: "+parameter;
+	}
+
+	@GetMapping("/users")
+	public ArrayList<String> giveUsers(){
+		return userList;
+	}
+
+	@RequestMapping("/users/{parameter}")
+	public String addUser(@PathVariable String parameter){
+		userList.add(parameter);
+		return "User "+ parameter+" wurde zu er Userliste hinzugefügt";
+		
 	}
 	
 
